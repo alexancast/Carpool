f62b923eea2707882bd13f817d2452274d8d9cb8
alexancast <37774672+alexancast@users.noreply.github.com>
2023-11-27 23:47:51 +0100

    Improved slide animation

--- JS/booking.js
+++ JS/booking.js
@@ -103,12 +103,12 @@ search.addEventListener("click", function (event) {
     const minDropoffTime = new Date(pickupDate);
     minDropoffTime.setMinutes(minDropoffTime.getMinutes() + 15);
 
-    if (dropoffDate <= pickupDate || dropoffDate < minDropoffTime) {
-        alert("Bil kan inte bokas kortare än 15 minuter.");
-    } else {
+    // if (dropoffDate <= pickupDate || dropoffDate < minDropoffTime) {
+    //     alert("Bil kan inte bokas kortare än 15 minuter.");
+    // } else {
 
-        searchCars();
-    }
+    searchCars();
+    // }
 });
 
 
@@ -259,6 +259,10 @@ function createCarEntity(car, carKey, index) {
 
     // Append the car div to the parent container
     parentContainer.appendChild(carDiv);
+}
+
+function getRandomRotation() {
+    return (Math.random() * 10) - 5;
 }
 
 // Function to create and add car entities to the parent container
